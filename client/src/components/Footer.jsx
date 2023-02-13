037f1a19c7e316bba04aadf904064e24eb448704
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-13 13:40:48 +0200

    footer and design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Logo from '../assets/blogLogo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <img src={Logo} alt=''/>
+        <span>
+            Made with ❤ and <b>React.js</b>
+        </span>
+    </footer>
   )
 }
 
