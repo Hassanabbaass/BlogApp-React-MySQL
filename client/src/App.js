3abae3438aa474e3b794dbb698a6d1704dee048e
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-13 11:16:28 +0200

    pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,63 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+        <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Layout/>),
+    children: [
+      {
+        path: "/",
+        element: (<Home/>)
+      },
+      {
+        path: "/post/:id",
+        element: (<Single/>)
+      },
+      {
+        path: "/write",
+        element: (<Write/>)
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: (<Register/>),
+  },
+  {
+    path: "/login",
+    element: (<Login/>),
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
