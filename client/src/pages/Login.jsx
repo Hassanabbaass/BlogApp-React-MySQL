91bf97ab88ad616469528f0fc6693f6d3f38d59f
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-13 11:45:51 +0200

    login page and style

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='loginDiv'>
+        <h1>Login</h1>
+        <form>
+            <input type='text' placeholder='username' />
+            <input type='password' placeholder='password' />
+            <button>Login</button>
+            <p>This is an error</p>
+            <span>Dont have an account? <Link to='/register'>Register</Link></span>
+        </form>
+    </div>
   )
 }
 
