a60f3a7af01bf56e0b272ab44238054e5cff9c72
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-13 11:50:40 +0200

    login and registration pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='loginDiv'>
+    <div className='authDiv'>
         <h1>Login</h1>
         <form>
-            <input type='text' placeholder='username' />
-            <input type='password' placeholder='password' />
+            <input required type='text' placeholder='username' />
+            <input required type='password' placeholder='password' />
             <button>Login</button>
             <p>This is an error</p>
             <span>Dont have an account? <Link to='/register'>Register</Link></span>
