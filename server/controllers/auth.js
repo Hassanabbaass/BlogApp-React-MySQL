98466b88a284b1feaec71b1a43172c9c94d3b58d
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-16 15:14:59 +0200

    Login + jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const { db } = require("../db");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = (req,res) => {
     
@@ -30,7 +31,27 @@ const register = (req,res) => {
 }
 
 const login = (req,res) => {
+    //chech if user is found
+    const q = "SELECT * FROM users WHERE username = ?";
 
+    db.query(q, [req.body.username], (err, data) => {
+        if(err) return res.json(err);
+        if(data.length === 0 ) return res.status(404).json("User not found!");
+
+        //checking the hashed password
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+
+        const token = jwt.sign({ id: data[0].id}, "jwtkey")
+        const {password, ...other} = data[0];
+
+        res.cookie("access_token", token, {
+            httpOnly:true
+        })
+        .status(200).json(other)
+
+    })
 }
 
 const logout = (req,res) => {
